3b47aec52d27ec390f5bb9bb469355ccb4c0455e
viv0ivi <vivivivolkova@gmail.com>
2025-05-26 14:54:02 +0300

    Update okru_post_bot.py

--- okru_post_bot.py
+++ okru_post_bot.py
@@ -1,7 +1,7 @@
 import os
 import logging
-from telegram import Bot
-from telegram.ext import Application, CommandHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardButton, ReplyKeyboardMarkup
+from telegram.ext import Application, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 
 # Настройки из ENV
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
@@ -15,13 +15,49 @@ logging.basicConfig(format="%(asctime)s | %(levelname)s | %(message)s", level=lo
 
 # Асинхронная функция для команды /start
 async def cmd_start(update, context):
-    await update.message.reply_text("Тест")
+    # Inline клавиатура
+    inline_keyboard = [
+        [InlineKeyboardButton("Кнопка 1", callback_data="button_1")],
+        [InlineKeyboardButton("Кнопка 2", callback_data="button_2")]
+    ]
+    inline_reply_markup = InlineKeyboardMarkup(inline_keyboard)
+
+    # Reply клавиатура
+    reply_keyboard = [
+        [ReplyKeyboardButton("Кнопка A"), ReplyKeyboardButton("Кнопка B")],
+        [ReplyKeyboardButton("Кнопка C")]
+    ]
+    reply_markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True)
+
+    # Отправляем сообщение с обеими клавишами
+    await update.message.reply_text("Выберите одну из кнопок:", reply_markup=inline_reply_markup)
+
+# Обработчик кнопок
+async def button_handler(update, context):
+    query = update.callback_query
+    await query.answer()
+    if query.data == "button_1":
+        await query.edit_message_text("Вы нажали кнопку 1")
+    elif query.data == "button_2":
+        await query.edit_message_text("Вы нажали кнопку 2")
+
+# Обработчик для кнопок Reply
+async def reply_button_handler(update, context):
+    text = update.message.text
+    if text == "Кнопка A":
+        await update.message.reply_text("Вы выбрали кнопку A")
+    elif text == "Кнопка B":
+        await update.message.reply_text("Вы выбрали кнопку B")
+    elif text == "Кнопка C":
+        await update.message.reply_text("Вы выбрали кнопку C")
 
 # Создание Telegram приложения
 application = Application.builder().token(TELEGRAM_TOKEN).build()
 
-# Регистрация обработчика для команды /start
+# Регистрация обработчиков
 application.add_handler(CommandHandler("start", cmd_start))
+application.add_handler(CallbackQueryHandler(button_handler))
+application.add_handler(MessageHandler(Filters.text & ~Filters.command, reply_button_handler))
 
 # Запуск бота
 if __name__ == "__main__":
